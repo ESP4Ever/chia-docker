5fc496c7724642f61f7ec275268dd773df6d2b59
ESP4Ever <44872879+ESP4Ever@users.noreply.github.com>
2021-05-31 12:59:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
+cd /chia-blockchain
+. ./activate
+
 if [[ ! "$(ls ${plots_dir}/*.plot)" ]]; then
   echo "${plots_dir} directory appears to be empty and you have not specified another, please mounting a plot directory "
 fi
+
+tail -f /dev/null
