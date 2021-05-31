4fd1caec7051f67f26245a806713f2550128a396
ESP4Ever <44872879+ESP4Ever@users.noreply.github.com>
2021-05-31 11:05:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,3 @@
-tail -f /dev/null
+if [[ ! "$(ls ${plots_dir}/*.plot)" ]]; then
+  echo "${plots_dir} directory appears to be empty and you have not specified another, please mounting a plot directory "
+fi
