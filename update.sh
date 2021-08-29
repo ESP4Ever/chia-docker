8e08b7b0aa9bc70b98bb2932c58c0da16dff3a94
ESP4Ever <44872879+ESP4Ever@users.noreply.github.com>
2021-08-29 18:57:56 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,16 +6,6 @@ deactivate
 git fetch
 git checkout latest
 git reset --hard FETCH_HEAD --recurse-submodules
-
-# If you get RELEASE.dev0 then delete the package-lock.json in chia-blockchain-gui and install.sh again
-
-git status
-
-# git status should say "nothing to commit, working tree clean", 
-# if you have uncommitted changes, RELEASE.dev0 will be reported.
-
 sh install.sh
-
 . ./activate
-
 chia init
